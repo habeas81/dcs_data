7bd5bb02c71091c1a64136d3176f41e69c7b1bb4
Alex Robinson <girotobial@gmail.com>
2023-08-10 14:56:57 +0100

    add staging view for kills

---
+++ models/staging/stg_sdcs__kills.sql
@@ -0,0 +1,136 @@
+with
+
+source as (
+    select * from {{ ref('base__kill_events')}}
+),
+
+clean as (
+    select
+        
+        -- primary_id
+        event_id,
+
+        -- foreign_ids
+        campaign_id,
+        initiator_id::int as initiator_id,
+        initiator_user_id,
+        initiator_group_id::int as initiator_group_id,
+        target_id::int as target_id,
+        target_user_id,
+        target_group_id::int as target_group_id,
+        weapon_id::int as weapon_id,
+
+        -- datetime
+        time_created as datetime_created,
+        time_created::date as date_created,
+        time_created::time as time_created,
+
+        -- boolean
+        initiator_in_air::boolean as initiator_started_in_air,
+        target_in_air::boolean as target_started_in_air,
+
+        -- text
+        initiator_name,
+        initiator_type,
+        initiator_group_name,
+        split_part(initiator_group_category, '_', 3) as initiator_group_category,
+        split_part(initiator_group_coalition, '_', 2) as initiator_group_coalition,
+        {{ blanks_as_null('initiator_callsign') }} as initiator_callsign,
+        initiator_category,
+        split_part(initiator_coalition, '_', 2) as initiator_coalition,
+        {{ blanks_as_null('initiator_player_name') }} as initiator_player_name,
+        target_name,
+        target_group_name,
+        split_part(target_group_category, '_', 3) as target_group_category,
+        split_part(target_group_coalition, '_', 2) as target_group_coalition,
+        {{ blanks_as_null('target_callsign') }} as target_callsign,
+        target_category,
+        split_part(target_coalition, '_', 2) as target_coalition,
+        {{ blanks_as_null('target_player_name') }} as target_player_name,
+        target_type,
+        {{ blanks_as_null('weapon_name') }} as weapon_name,
+        weapon_type,
+
+        -- integers
+        initiator_number_in_group::int as initiator_number_in_group,
+        target_number_in_group::int as target_number_in_group,
+
+        -- float
+        initiator_fuel::float as initiator_fuel,
+        (initiator_position_u::float) as initiator_position_u,
+        initiator_position_v::float as initiator_position_v,
+        initiator_position_alt::float as initiator_position_altitude,
+        initiator_position_lat::float as initiator_position_latitude,
+        initiator_position_lon::float as initiator_position_longitude,
+        initiator_velocity_speed::float as initiator_velocity_speed,
+        initiator_velocity_heading::float as initiator_velocity_heading,
+        initiator_velocity_x::float as initiator_velocity_x,
+        initiator_velocity_y::float as initiator_velocity_y,
+        initiator_orientation_up_x::float as initiator_orientation_up_x,
+        initiator_orientation_up_y::float as initiator_orientation_up_y,
+        initiator_orientation_up_z::float as initiator_orientation_up_z,
+        initiator_orientation_yaw::float as initiator_orientation_yaw,
+        initiator_orientation_roll::float as initiator_orientation_roll,
+        initiator_orientation_pitch::float as initiator_orientation_pitch,
+        initiator_orientation_right_x::float as initiator_orientation_right_x,
+        initiator_orientation_right_y::float as initiator_orientation_right_y,
+        initiator_orientation_right_z::float as initiator_orientation_right_z,
+        initiator_orientation_forward_x::float as initiator_orientation_forward_x,
+        initiator_orientation_forward_y::float as initiator_orientation_forward_y,
+        initiator_orientation_forward_z::float as initiator_orientation_forward_z,
+        initiator_orientation_heading::float as initiator_orientation_heading,
+        target_fuel::float as target_fuel,
+        target_position_u::float as target_position_u,
+        target_position_v::float as target_position_v,
+        target_position_alt::float as target_position_altitude,
+        target_position_lat::float as target_position_latitude,
+        target_position_lon::float as target_position_longitude,
+        target_velocity_speed::float as target_velocity_speed,
+        target_velocity_heading::float as target_velocity_heading,
+        target_velocity_x::float as target_velocity_x,
+        target_velocity_y::float as target_velocity_y,
+        target_orientation_up_x::float as target_orientation_up_x,
+        target_orientation_up_y::float as target_orientation_up_y,
+        target_orientation_up_z::float as target_orientation_up_z,
+        target_orientation_yaw::float as target_orientation_yaw,
+        target_orientation_roll::float as target_orientation_roll,
+        target_orientation_pitch::float as target_orientation_pitch,
+        target_orientation_right_x::float as target_orientation_right_x,
+        target_orientation_right_y::float as target_orientation_right_y,
+        target_orientation_right_z::float as target_orientation_right_z,
+        target_orientation_forward_x::float as target_orientation_forward_x,
+        target_orientation_forward_y::float as target_orientation_forward_y,
+        target_orientation_forward_z::float as target_orientation_forward_z,
+        target_orientation_heading::float as target_orientation_heading,
+        weapon_position_u::float as weapon_position_u,
+        weapon_position_v::float as weapon_position_v,
+        weapon_position_alt::float as weapon_position_altitude,
+        weapon_position_lat::float as weapon_position_latitude,
+        weapon_position_lon::float as weapon_position_longitude,
+        weapon_velocity_speed::float as weapon_velocity_speed,
+        weapon_velocity_heading::float as weapon_velocity_heading,
+        weapon_velocity_velocity_x::float as weapon_velocity_velocity_x,
+        weapon_velocity_velocity_y::float as weapon_velocity_velocity_y,
+        weapon_velocity_velocity_z::float as weapon_velocity_velocity_z,
+        weapon_orientation_up_x::float as weapon_orientation_up_x,
+        weapon_orientation_up_y::float as weapon_orientation_up_y,
+        weapon_orientation_up_z::float as weapon_orientation_up_z,
+        weapon_orientation_yaw::float as weapon_orientation_yaw,
+        weapon_orientation_roll::float as weapon_orientation_roll,
+        weapon_orientation_pitch::float as weapon_orientation_pitch,
+        weapon_orientation_right_x::float as weapon_orientation_right_x,
+        weapon_orientation_right_y::float as weapon_orientation_right_y,
+        weapon_orientation_right_z::float as weapon_orientation_right_z,
+        weapon_orientation_forward_x::float as weapon_orientation_forward_x,
+        weapon_orientation_forward_y::float as weapon_orientation_forward_y,
+        weapon_orientation_forward_z::float as weapon_orientation_forward_z,
+        weapon_orientation_heading::float as weapon_orientation_heading
+
+        from source
+),
+
+final as (
+    select * from clean
+)
+
+select * from final
