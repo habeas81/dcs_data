9f9af4e1c3c5f4a2c0bf0effd72781c193846aa8
Alex Robinson <girotobial@gmail.com>
2023-08-16 20:13:39 +0100

    use bigint in int_or

--- macros/udfs/f_int_or.sql
+++ macros/udfs/f_int_or.sql
@@ -1,7 +1,7 @@
 {% macro create_f_int_or_null() %}
 CREATE OR REPLACE FUNCTION {{target.schema}}.int_or_null(v_input text)
-RETURNS INTEGER AS $$
-DECLARE v_int_value INTEGER DEFAULT NULL;
+RETURNS BIGINT AS $$
+DECLARE v_int_value BIGINT DEFAULT NULL;
 BEGIN
     BEGIN
         v_int_value := v_input::INTEGER;
