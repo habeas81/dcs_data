62686070c8e19d16a76a3acab12faea1aabbbd66
Alex Robinson <girotobial@gmail.com>
2023-08-16 15:26:32 +0100

    :sparkles: add unit types staging table

---
+++ models/staging/stg_sdcs__unit_types.sql
@@ -0,0 +1,49 @@
+with
+
+source as (
+    select * from {{source('sdcs', 'unit_type')}}
+),
+
+clean as (
+    select
+        -- ids
+        id,
+        unpack_type_id,
+
+        -- text
+        jtac_aggr_category as jtac_aggregate_category,
+        shape_name,
+        initcap(split_part(skill::text, '_', 2)) as skill,
+        type_name,
+        unit_type,
+        initcap(unit_class::text) as unit_class,
+        unpack_composition,
+
+        -- boolean
+        is_static,
+        can_sling,
+        can_drive,
+        is_slot,
+
+        -- int
+        cargo_capacity,
+        cargo_count,
+        cargo_max_level,
+        cargo_max_persist_level,
+        factory_level,
+        jtac_priority
+        load_delay,
+        mass_kg,
+        shelter_level,
+        troop_count,
+        unit_level,
+        unit_cost
+
+    from source
+),
+
+final as (
+    select * from clean
+)
+
+select * from final
