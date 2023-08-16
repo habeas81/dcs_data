15acc6ebbc5df62055a19e8435a26fe080143c37
Paul F <habeas81@users.noreply.github.com>
2023-08-16 12:30:15 -0400

    initial 'shot' events

---
+++ models/staging/shot_events/base__shot_weapons.sql
@@ -0,0 +1,45 @@
+{{
+    config(
+        materialized="ephemeral"
+    )
+}}
+with
+
+shots as (
+    select * from {{ ref('base__shot_events') }}
+),
+
+columns as (
+    select
+        shot_id,
+        weapon
+    from shots
+),
+
+flatten as (
+    select
+        shot_id,
+        (weapon ->> 'id')::int as weapon_id,
+        weapon ->> 'type' as weapon_type,
+        (weapon -> 'position' ->> 'u')::float as weapon_position_u,
+        (weapon -> 'position' ->> 'v')::float as weapon_position_v,
+        (weapon -> 'position' ->> 'alt')::float as weapon_altitude,
+        (weapon -> 'position' ->> 'lat')::float as weapon_latitude,
+        (weapon -> 'position' ->> 'lon')::float as weapon_longitude,
+        (weapon -> 'velocity' ->> 'speed')::float as weapon_speed,
+        (weapon -> 'velocity' ->> 'heading')::float as weapon_heading
+    from columns
+),
+
+filtered as (
+    select
+        *
+    from flatten
+    where NOT flatten.weapon_id IS NULL
+),
+
+final as (
+    select * from filtered
+)
+
+select * from final
